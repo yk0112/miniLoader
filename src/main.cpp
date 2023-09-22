8378f0118c8e868fba92b94e356bb23f4f3f8cc1
yk0112 <yosikou0726@gmail.com>
2023-09-23 00:25:48 +0900

    refactor: ポインタ管理の改善

---
+++ src/main.cpp
@@ -0,0 +1,49 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include "../include/helper.hpp"
+#include <memory>
+
+int main(int argc, char *argv[])
+{
+  if (argc != 2)
+  {
+    std::cerr << "Please specify the appropriate arguments";
+    return 1;
+  }
+
+    const char *filename = argv[1];
+    std::ifstream file(filename, std::ios::binary | std::ios::ate);
+
+    if (!file) {
+      std::cerr << "Failed to open file: " << filename << std::endl;
+      return 1;
+    }
+
+    int size = file.tellg();
+
+    file.seekg(0, std::ios_base::beg);
+
+    auto buf = std::make_unique<std::vector<char>>(size);
+
+    file.read(buf->data(), size);
+
+  const char *_argv[] = {
+      argv[0],
+      "arg1",
+      "arg2",
+      NULL,
+  };
+
+  const char *_env[] = {
+      "HOME=/tmp",
+      NULL,
+  };
+
+  execute_elf(std::move(buf), _argv, _env);
+
+  return 0;
+}
