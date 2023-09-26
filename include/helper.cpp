fdf81cac3b8427dd6316ced919990b6e11d19c12
yk0112 <yosikou0726@gmail.com>
2023-09-27 00:06:11 +0900

    feat: 共有ライブラリのロードを対応

--- include/helper.cpp
+++ include/helper.cpp
@@ -1,5 +1,5 @@
 #include "helper.hpp"
-
+#include <fstream>
 #include <elf.h>
 #include <vector>
 #include <type_traits>
@@ -9,11 +9,13 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <unistd.h>
-
 #include <iostream>
+#include <vector>
 #include <string>
 #include <memory>
 #include "../include/helper.hpp"
+
+typedef int (*func_t)(int, char **, char **);
 
 bool is_elf(Elf_Ehdr *Ehdr)
 {
@@ -30,7 +32,24 @@ bool is_elf(Elf_Ehdr *Ehdr)
   }
 }
 
-typedef void (*func_t)();
+static Elf_Phdr *search_phdr(Elf_Ehdr *Ehdr, unsigned int type)
+{
+  char *head = reinterpret_cast<char *>(Ehdr);
+
+  for (int i = 0; i < Ehdr->e_phnum; i++)
+  {
+    Elf_Phdr *Phdr = reinterpret_cast<Elf_Phdr *>(head + Ehdr->e_phoff + Ehdr->e_phentsize * i);
+
+    if (Phdr->p_type == type)
+    {
+      std::cout << "find phdr, type:" << type << "\n";
+      return reinterpret_cast<Elf_Phdr *>(head + Phdr->p_offset);
+    }
+  }
+  std::cout << "can not find phdr"
+            << "\n";
+  return nullptr;
+}
 
 static Elf_Shdr *search_shdr(Elf_Ehdr *Ehdr, const char *name)
 {
@@ -55,13 +74,11 @@ static Elf_Shdr *search_shdr(Elf_Ehdr *Ehdr, const char *name)
   exit(1);
 }
 
-static void load_file(std::vector<char> &head, size_t *entry) 
+static void load_file(std::vector<char> &head, Elf_Ehdr *Ehdr, size_t *base_addr, size_t *entry)
 {
-  Elf_Ehdr *Ehdr;
   Elf_Phdr *Phdr;
-  Elf_Shdr *Shdr; 
- 
-  Ehdr = reinterpret_cast<Elf_Ehdr*>(head.data());
+  Elf_Shdr *Shdr;
+  func_t f;
 
   if (!is_elf(Ehdr))
   {
@@ -81,19 +98,21 @@ static void load_file(std::vector<char> &head, size_t *entry)
     exit(1);
   }
 
-  intptr_t base;
+  size_t base;
 
   if (Ehdr->e_type == ET_DYN)
   {
     // DYNの場合ランダムなベースアドレスを与える
     base = reinterpret_cast<intptr_t>(mmap(0, 100 * PAGE_SIZE,
-                                           PROT_READ | PROT_WRITE,
+                                           PROT_READ | PROT_WRITE | PROT_EXEC,
                                            MAP_PRIVATE | MAP_ANON, -1, 0));
     munmap(reinterpret_cast<void *>(base), 100 * PAGE_SIZE);
   }
   else
     base = 0;
-  
+
+  *entry = base + Ehdr->e_entry;
+
   Elf_Phdr *Phdr_tmp;
 
   // 各セグメントをメモリ上に展開
@@ -101,20 +120,27 @@ static void load_file(std::vector<char> &head, size_t *entry)
   {
     Phdr_tmp = reinterpret_cast<Elf_Phdr *>(head.data() + Ehdr->e_phoff + Ehdr->e_phentsize * i);
 
-    if (Phdr_tmp->p_type == PT_LOAD || Phdr_tmp->p_type == PT_INTERP)
+    if (Phdr_tmp->p_type == PT_LOAD)
     {
       void *map_start = reinterpret_cast<void *>(
           ROUND_DOWN(Phdr_tmp->p_vaddr,
                      PAGE_SIZE)); // ページサイズのアラインメントに従った配置
+
       auto round_down_size = static_cast<int>(
           Phdr_tmp->p_vaddr - reinterpret_cast<intptr_t>(map_start));
 
       auto map_size = ROUND_UP(Phdr_tmp->p_memsz + round_down_size, PAGE_SIZE);
 
-      mmap(map_start + base, map_size, PROT_READ | PROT_WRITE,
+      mmap(map_start + base, map_size, PROT_READ | PROT_WRITE | PROT_EXEC,
            MAP_PRIVATE | MAP_ANON | MAP_FIXED, -1, 0);
       memcpy(reinterpret_cast<void *>(base) + Phdr_tmp->p_vaddr,
              head.data() + Phdr_tmp->p_offset, Phdr_tmp->p_filesz);
+
+      void *st = map_start + base;
+      __builtin___clear_cache(st, st + map_size);
+
+      size_t start = reinterpret_cast<size_t>(map_start) + base;
+      *base_addr = std::min(*base_addr, start);
 
       std::cout << "file loaded. \n";
     }
@@ -124,23 +150,90 @@ static void load_file(std::vector<char> &head, size_t *entry)
     }
   }
 
- // bss領域をゼロクリア
- // Shdr.reset(search_shdr(Ehdr.get(), ".bss"));
-
- Shdr = search_shdr(Ehdr, ".bss");
+  // bss領域をゼロクリア
+  Shdr = search_shdr(Ehdr, ".bss");
 
   if (Shdr != nullptr)
   {
     memset((char *)Shdr->sh_addr + base, 0, Shdr->sh_size);
     std::cout << "clear BSS section. \n";
   }
-
 }
 
-void execute_elf(std::unique_ptr<std::vector<char>> &&buf, const char *argv[], const char *env[])
+
+
+void execute_elf(std::unique_ptr<std::vector<char>> &&buf, char *argv[], char *env[])
 {
   auto head = std::move(buf);
-  size_t entry;
-  load_file(*head, &entry);
-  
+  size_t entry = MAX;        // エントリーポイントのアドレス(アラインメント制約を考慮していない？)
+  size_t base = MAX;         // 最初のセグメントのアドレス(アラインメント制約を考慮)
+  size_t interp_entry = MAX; //  動的リンカー用
+  size_t interp_base = MAX;  // 動的リンカー用
+  int argc = 0;
+  int envc = 0;
+  int str_ptr = 0;
+  int stack_ptr = 1;
+  auto Ehdr = reinterpret_cast<Elf_Ehdr *>(head->data());
+
+  while (argv[argc] != nullptr)
+  {
+    ++argc;
+  }
+
+  while (env[envc] != nullptr)
+  {
+    ++envc;
+  }
+
+  std::vector<char *> stack(STACK_SIZE);
+
+  load_file(*head, Ehdr, &base, &entry);
+
+  Elf_Phdr *interp = search_phdr(Ehdr, 3);
+
+  // 動的リンカの使用あり
+  if (interp != nullptr)
+  {
+    char *loader_path = reinterpret_cast<char *>(interp);
+
+    std::ifstream file(loader_path, std::ios::binary | std::ios::ate);
+
+    if (!file)
+    {
+      std::cerr << "Failed to open file: " << loader_path << std::endl;
+      exit(1);
+    }
+
+    int size = file.tellg();
+    file.seekg(0, std::ios_base::beg);
+
+    std::vector<char> file_cont(size);
+    file.read(file_cont.data(), size);
+
+    auto interp_Ehdr = reinterpret_cast<Elf_Ehdr *>(file_cont.data());
+    load_file(file_cont, interp_Ehdr, &interp_base, &interp_entry);
+  }
+
+  for (int i = 0; i < argc; i++)
+  {
+    stack[i] = argv[i];
+    stack_ptr++;
+  }
+
+  Elf_Shdr *init = search_shdr(Ehdr, ".init");             // startup routine
+  Elf_Shdr *init_array = search_shdr(Ehdr, ".init_array"); // Array of function pointer
+
+  func_t st_routine = reinterpret_cast<func_t>(base + init->sh_addr);
+  st_routine(argc, argv, env); // eroor
+  std::cout << "good! \n";
+
+  for (int i = 0; i < init_array->sh_size / PTR_SIZE; i++)
+  {
+    long *gl_ptr = reinterpret_cast<long *>(base + init_array->sh_addr + i * PTR_SIZE);
+    func_t gl_constructor = reinterpret_cast<func_t>(base + *gl_ptr);
+    gl_constructor(argc, argv, env);
+    std::cout << "good!! \n";
+  }
+
+  exit(1);  
 }
--- include/helper.hpp
+++ include/helper.hpp
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <memory>
 #include <vector>
+#include <limits>
 
 #if INTPTR_MAX == INT32_MAX
 		typedef Elf32_Ehdr Elf_Ehdr;
@@ -12,6 +13,7 @@
 		typedef Elf32_Word Elf_Word;
 		#define ELF_R_SYM(x) ELF32_R_SYM(x)
 		#define ELF_R_TYPE(x) ELF32_R_TYPE(x)
+		#define PTR_SIZE 4
 #elif INTPTR_MAX == INT64_MAX
 		typedef Elf64_Ehdr Elf_Ehdr;
 		typedef Elf64_Phdr Elf_Phdr;
@@ -21,20 +23,25 @@
 		typedef Elf64_Word Elf_Word;
 		#define ELF_R_SYM(x) ELF64_R_SYM(x)
 		#define ELF_R_TYPE(x) ELF64_R_TYPE(x)
+		#define PTR_SIZE 8
 #else
 		#error("not found ELF format.")
 #endif
 
+#define STACK_SIZE (8*1024*1024)
 
 #define PAGE_SIZE (sysconf(_SC_PAGESIZE))
-
+#define MAX std::numeric_limits<size_t>::max();
 #define ROUND_UP(v, s) ((v + s - 1) & -s)
 #define ROUND_DOWN(v, s) (v & -s)
 
-bool is_elf(std::unique_ptr<Elf_Ehdr> Ehdr);
+
+bool is_elf(Elf_Ehdr *Ehdr);
 
 static Elf_Shdr* search_shdr(Elf_Ehdr* Ehdr, const char *name);
 
-static void load_file(std::vector<char> &head, size_t* entry);
+static void load_file(std::vector<char> &head, Elf_Ehdr* Ehdr, size_t *base_addr, size_t* entry);
 
-void execute_elf(std::unique_ptr<std::vector<char>> &&buf, const char* argv[], const char* env[]);
+void execute_elf(std::unique_ptr<std::vector<char>> &&buf,char *argv[] ,char *env[]);
+
+void jump_target(long *init, void *exit_func, void *entry);
